83a8f506e828e3bd3082a2114af661b89051f45e
Nickolas Alcazar <nickolasalcazar@nickolass-air.attlocal.net>
2021-04-13 13:39:35 -0700

    Started app users

--- secretsanta/secretsanta/settings.py
+++ secretsanta/secretsanta/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 # For hiding sensitive info on GitHub; senstive info enclosed in decouple_config()
-from decouple import config as decouple_config
+from decouple import config as decouple_config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,14 +74,12 @@ WSGI_APPLICATION = 'secretsanta.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
