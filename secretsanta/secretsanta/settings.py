a662b93ece018971ba1e1c31a64ceb22a92096b4
Nickolas Alcazar <nickolasalcazar@nickolass-air.attlocal.net>
2021-04-13 13:05:35 -0700

    Added requirements.txt

--- secretsanta/secretsanta/settings.py
+++ secretsanta/secretsanta/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+# For hiding sensitive info on GitHub; senstive info enclosed in decouple_config()
+from decouple import config as decouple_config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ew+yr6o)gvmjcj5c*$(!fe)ogtj_mae&g3&t71^=9)s9$h*9@6'
+
+SECRET_KEY = decouple_config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = decouple_config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = decouple_config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
