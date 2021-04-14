aedab479f929e4672e1588c0a89e8d2d7dc8b49e
Nickolas Alcazar <nickolasalcazar@nickolass-air.attlocal.net>
2021-04-13 20:12:41 -0700

    Added signals for creating a Profile object upon the creation of a new User object.

--- secretsanta/secretsanta/settings.py
+++ secretsanta/secretsanta/settings.py
@@ -17,10 +17,8 @@ from decouple import config as decouple_config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 
 SECRET_KEY = decouple_config('SECRET_KEY')
 
--- secretsanta/users/views.py
+++ secretsanta/users/views.py
@@ -4,18 +4,15 @@ from .forms import UserRegisterForm
 
 def register(request):
 	if request.method == 'POST':
-		# Create a UserCreationForm with the data from the POST request
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-
-			messages.success(request, 'Your account as been created, you may now log in')
+			messages.success(request, f'Your account as been created, {username}')
 
 			#return redirect('login')
 			return redirect('/')
 	else:
-		# Else create an empty form
 		form = UserRegisterForm()
 
 	return render(request, 'users/register.html', {'form': form})
