a2decf37fca2832d6115032202605563d11b2413
Nickolas Alcazar <nickolasalcazar@nickolass-air.attlocal.net>
2021-05-05 09:47:46 -0700

    Working on adding/removing forms dynamically within game_form.js and game_form.html. Not working out so well.

--- secretsanta/game/static/game/game_form.js
+++ secretsanta/game/static/game/game_form.js
@@ -1,3 +1,70 @@
 // JS for templates/game/game_form.html
+console.log("game/game_form.js")
 
-console.log("Working")
+
+
+
+player_forms = document.getElementsByClassName('player_form')
+
+
+/*
+ * Add extra field
+ */
+function addForm() {
+    let clone = player_forms[player_forms.length-1].cloneNode(true)
+
+    console.log(clone)
+
+    togglePlayerFormeButton(clone)
+    player_forms[player_forms.length-1].parentNode.appendChild(clone)
+}
+
+function deleteForm() {
+
+
+}
+
+function togglePlayerFormeButton(elem) {
+    elem.addEventListener('click', function() {
+
+        if (this.classList.contains('add-form-row')) {
+            this.innerHTML = '-'
+            addForm()
+
+        } else {
+            //this.innerHTML = '+'
+            //deleteForm()
+            this.parentNode.remove()
+        }
+
+        this.classList.toggle('add-form-row')
+        this.classList.toggle('remove-form-row')
+    })
+}
+
+player_btns = document.getElementsByClassName('player-btn')
+
+for (i = 0; i < player_btns.length; i++) {
+    togglePlayerFormeButton(player_btns[i])
+}
+
+/*
+for (i = 0; i < player_btns.length; i++) {
+    player_btns[i].addEventListener('click', function() {
+
+        if (this.classList.contains('add-form-row')) {
+            this.innerHTML = '-'
+            addForm()
+
+        } else {
+            this.innerHTML = '+'
+            deleteForm()
+
+        }
+
+        this.classList.toggle('add-form-row')
+        this.classList.toggle('remove-form-row')
+    })
+}
+*/
+
