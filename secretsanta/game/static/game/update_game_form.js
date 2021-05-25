ace75430856726be75f605ea4ec27814aa158d1e
Nickolas Alcazar <nickolasalcazar@nickolass-air.attlocal.net>
2021-05-25 09:26:22 -0700

    updateGame form now dynamically displays messages specifying criteria for submitting a game.

--- secretsanta/game/static/game/update_game_form.js
+++ secretsanta/game/static/game/update_game_form.js
@@ -1,12 +1,17 @@
 // JS for templates/game/game_form.html
 console.log('game/update_game_form.js');
-console.log('cached 6')
+console.log('cached15')
 
 const add_player_btn = document.querySelector('#add-player-btn');
 
 const game_form = document.querySelector('#game-form');
 const player_form = document.querySelector('#add-player-form');
 const player_form_rows = document.getElementsByClassName('add-player-row'); // Returns live list
+
+// Criteria list variables
+const criteria_list = document.querySelector('#criteria-list');
+const odd_player_msg = document.querySelector('#odd-player-msg');
+const min_player_msg = document.querySelector('#min-player-msg');
 
 // Select Django form manager TOTAL_FORMS variable
 let totalForms = document.querySelector("#id_player_set-TOTAL_FORMS");
@@ -41,10 +46,7 @@ add_player_btn.addEventListener('click', function(event) {
     validateForm();
 });
 
-// Remove player-form-row
-// Event bubbling. If any child element of game_form is clicked,
-// then the event will be bubbled up to the parent node game_form.
-// The expression event.target refers to the element the event fired on.
+// Remove player-form-row. Event bubbling.
 game_form.addEventListener('click', function(event) {
     if (event.target.classList.contains('rmv-player-btn')) {
         event.preventDefault();
@@ -73,7 +75,7 @@ function countPlayers() {
     for (let form of player_form_rows) {
         // Ignore hidden forms
         if (window.getComputedStyle(form).display === 'none') continue;
-        // Grabs the 'name' field of each form and checks if it field i
+        // Grabs the 'name' field of each form and checks if filled in
         if (form.querySelector('input').value) count++;
     }
     return count;
@@ -99,6 +101,7 @@ function countForms() {
 function validateForm() { 
     const totalPlayers = countPlayers();
     submit_game_btn.disabled = !((totalPlayers % 2 == 0) && totalPlayers >= 4);
+    addMessages(totalPlayers);
 }
 
 /*
@@ -107,9 +110,16 @@ function validateForm() {
  */
 function canDeleteRow(formRow) { return countForms() >= 5; }
 
-
-
-
+/*
+ * Displays elements that specify critieria to creating a Game.
+ * Takes current total number of Players as an argument.
+ */
+function addMessages(totalPlayers) {
+    if (totalPlayers % 2 == 0) odd_player_msg.style.display = 'none';
+    else odd_player_msg.style.display = '';
+    if (totalPlayers >= 4) min_player_msg.style.display = 'none';
+    else min_player_msg.style.display = '';
+}
 
 
 
