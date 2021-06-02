7bb902b40687800feee01cefe7f53be6058425a4
Nickolas Alcazar <nickolasalcazar@nickolass-air.attlocal.net>
2021-06-02 12:38:08 -0700

    Added STATIC_ROOT to allow Heroku to serve static files

--- secretsanta/settings.py
+++ secretsanta/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Email Settings ###################################################
 EMAIL_USE_TLS = True
